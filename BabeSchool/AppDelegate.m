4d2d3e83e478913f96bd2e18dd8b8da620fb7d29
Truong Huu Thao <thaoth58@gmail.com>
2015-11-21 07:34:35 +0700

    update

--- BabeSchool/AppDelegate.m
+++ BabeSchool/AppDelegate.m
@@ -9,6 +9,7 @@
 #import "AppDelegate.h"
 #import "ListSchoolViewController.h"
 #import <MagicalRecord/MagicalRecord.h>
+#import <Parse/Parse.h>
 
 #define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 #define NAV_BAR_COLOR 0x0abd80
