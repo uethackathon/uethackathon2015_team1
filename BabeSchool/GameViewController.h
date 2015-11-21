7a86b5b2d07aba9ec325710809943c67cf5ff161
thjnh195 <thjnh195@gmail.com>
2015-11-21 07:35:29 +0700

    create game

---
+++ BabeSchool/GameViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameViewController.h
+//  BabeSchool
+//
+//  Created by thjnh195 on 11/21/15.
+//  Copyright © 2015 Nguyễn Chí Hoàng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
---
+++ BabeSchool/GameViewController.m
@@ -0,0 +1,37 @@
+//
+//  GameViewController.m
+//  BabeSchool
+//
+//  Created by thjnh195 on 11/21/15.
+//  Copyright © 2015 Nguyễn Chí Hoàng. All rights reserved.
+//
+
+#import "GameViewController.h"
+
+@interface GameViewController ()
+
+@end
+
+@implementation GameViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
